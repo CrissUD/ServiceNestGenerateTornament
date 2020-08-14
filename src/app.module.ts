4f405783de0b42b5c12cac6c94c6a1566ba32306
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 08:26:17 -0500

    generacion de controladores y servicios para gestion de team

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { TournamentController } from './controllers/tournament/tournament.controller';
 import { TournamentService } from './services/tournament/tournament.service';
 import { FirebaseService } from './services/firebase/firebase.service';
+import { TeamController } from './controllers/team/team.controller';
+import { TeamService } from './services/team/team.service';
 
 @Module({
   imports: [],
-  controllers: [AppController,TournamentController],
-  providers: [AppService, TournamentService, FirebaseService],
+  controllers: [AppController,TournamentController, TeamController],
+  providers: [AppService, TournamentService, FirebaseService, TeamService],
 })
 export class AppModule {}
