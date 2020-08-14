251631a09dec83640cf860cc716d890194ef72ab
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 16:23:12 -0500

    Excepcion en appcontroller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import { FirebaseService } from './services/firebase/firebase.service';
 export class AppController {
   constructor(private readonly appService: AppService, private readonly firebaseService: FirebaseService) {}
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   @Get()
   getHello() {
     return "hello World";
