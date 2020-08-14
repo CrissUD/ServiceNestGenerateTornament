9906479512c3a3f9c3c96cb86aca899832d13271
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 07:14:47 -0500

    se establece conexion con firebase

--- src/services/firebase/firebase.service.ts
+++ src/services/firebase/firebase.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+import * as firebase from "firebase-admin";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const serviceAccount = require("../../../soccerCup.json");
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://soccercup-b6117.firebaseio.com"
+});
+
 @Injectable()
 export class FirebaseService {}
