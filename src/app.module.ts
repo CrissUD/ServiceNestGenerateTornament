242009e8d3dd169ff6ab94173aa2aa528c343fe8
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 07:06:30 -0500

    creacion de servicio encargado de conexion con BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TournamentController } from './controllers/tournament/tournament.controller';
 import { TournamentService } from './services/tournament/tournament.service';
+import { FirebaseService } from './services/firebase/firebase.service';
 
 @Module({
   imports: [],
   controllers: [AppController,TournamentController],
-  providers: [AppService, TournamentService],
+  providers: [AppService, TournamentService, FirebaseService],
 })
 export class AppModule {}
